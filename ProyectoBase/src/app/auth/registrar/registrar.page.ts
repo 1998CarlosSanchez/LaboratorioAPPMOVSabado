1cbdd72ae11f0b6de132ed65eb4fd17405cb34dd
Carlos <1998carlossv1998@gmail.com>
2019-09-28 08:59:33 -0500

    Practica login

--- ProyectoBase/src/app/auth/registrar/registrar.page.ts
+++ ProyectoBase/src/app/auth/registrar/registrar.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+/*import { RegistrarService } from './registrar.service';
+import { Usuario} */
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.page.html',
   styleUrls: ['./registrar.page.scss'],
 })
 export class RegistrarPage implements OnInit {
+  registrarForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder : FormBuilder) { 
+    this.registrarForm = this.formBuilder.group({
+      nombre: ['',Validators.required],
+      correo: ['',[Validators.required, Validators.email]],
+      contrasena: ['',[Validators.required, Validators.minLength(6)]],
+      confirmacionContrasena: ['',Validators.required]
+    });
+  }
 
   ngOnInit() {
+  }
+  get f() {return this.registrarForm.controls;}
+
+  registrar() {
+    console.log(this.registrarForm.valid);
   }
 
 }
