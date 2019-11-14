3e1e37e1512749c2595a5f24289377fc6547d1d0
Carlos <1998carlossv1998@gmail.com>
2019-11-13 22:18:15 -0600

    Movimientos y resumen

--- ProyectoBase/src/app/app.module.ts
+++ ProyectoBase/src/app/app.module.ts
@@ -8,15 +8,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import  {HttpClientModule} from '@angular/common/http';
-import {RegistrarPage} from './auth/registrar/registrar.page';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {HttpClientModule} from '@angular/common/http';
+import { RegistrarPage } from './auth/registrar/registrar.page';
 import { LoginPage } from './auth/login/login.page';
 
+import { ChartsModule } from 'ng2-charts';
+
+
 @NgModule({
-  declarations: [ AppComponent, RegistrarPage,LoginPage],
+  declarations: [ AppComponent, RegistrarPage, LoginPage],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  imports: [BrowserModule,
+            IonicModule.forRoot(),
+            AppRoutingModule,
+            FormsModule,
+            ReactiveFormsModule,
+            HttpClientModule,
+            ChartsModule,
+          ],
   providers: [
     StatusBar,
     SplashScreen,
